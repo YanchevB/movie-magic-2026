a0cb63fcccfdd3bedc7b030383a71bde7a535c3b
Boyan Yanchev <byanchev6@gmail.com>
2026-06-29 16:49:09 +0300

    fix: slight corrections for debugging

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (res, req) => {
-    req.send('Hello world!');
+app.get('/', (req, res) => {
+    res.send('Hello world!');
 })
 
-app.listen(3000, () => 
+app.listen(3000, () =>
     console.log('Server is running on http://localhost:3000...'));
