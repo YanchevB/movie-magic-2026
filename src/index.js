86ef06decda801a37cab12ee32b4e3073d68385a
Boyan Yanchev <byanchev6@gmail.com>
2026-06-29 16:33:07 +0300

    feat: express server is configured

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world!')
+import express from 'express';
+
+const app = express();
+
+app.get('/', (res, req) => {
+    req.send('Hello world!');
+})
+
+app.listen(3000, () => 
+    console.log('Server is running on http://localhost:3000...'));
