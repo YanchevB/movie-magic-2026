c7ab376260514919b8cdd95c62087fecdf776c03
Boyan Yanchev <byanchev6@gmail.com>
2026-07-13 16:39:16 +0300

    feat: add artist creation and write data in db

---
+++ src/repositories/artistRepository.js
@@ -0,0 +1,15 @@
+import { prisma } from '../lib/prisma.js'
+
+async function create(artistData) {
+    const artist = await prisma.artist.create({
+        data: artistData,
+    });
+
+    return artist;
+}
+
+const artistRepository = {
+    create
+}
+
+export default artistRepository
