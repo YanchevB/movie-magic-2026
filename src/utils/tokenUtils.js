b1414f9c47fbfebe62c2a7d9f668f543a2fcfaf0
Boyan Yanchev <byanchev6@gmail.com>
2026-07-25 13:42:26 +0300

    fix: hide the secret key

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -3,10 +3,7 @@ import jwt from "jsonwebtoken";
 export function generateAuthToken(user) {
     // Issue token
     const payload = { id: user.id, email: user.email}
-
-    const secret = 'SECRET_KEY'
-
-    // TODO fix the secret key
+    const secret = process.env.AUTH_SECRET;
     const token = jwt.sign(payload, secret, { expiresIn: '1h' });
 
     return token;
