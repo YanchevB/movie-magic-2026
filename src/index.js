69b1acd8e90317ed9c3d3693eedafb3039493d4a
Boyan Yanchev <byanchev6@gmail.com>
2026-07-26 13:23:16 +0300

    refine: restructure helpers in a seperate file system

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import { engine } from 'express-handlebars';
 import routes from './routes.js';
 import cookieParser from 'cookie-parser';
 import { authMiddleware } from './middlewares/authMiddleware.js';
+import * as helpers from './views/helpers'
 
 const app = express();
 
 // Setup Handlebars
 app.engine('hbs', engine({
     extname: 'hbs',
-    helpers: {
-        setTitle(title) {
-            this.pageTitle = title;
-        }
-    }
+    helpers
 }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
---
+++ src/views/helpers/index.js
@@ -0,0 +1,1 @@
+export * from './pageHelpers.js'
